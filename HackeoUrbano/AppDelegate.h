4e9eaf0231dc8416524e2ee2aaeebc7abfdaa7cc
devvlade <dev.vlade@gmail.com>
2016-03-05 17:03:31 -0600

    Initial Commit

---
+++ HackeoUrbano/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackeoUrbano
+//
+//  Created by M on 3/5/16.
+//  Copyright © 2016 Krieger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
